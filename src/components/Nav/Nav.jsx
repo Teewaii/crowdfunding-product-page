5b869765e40e2ce3b7621a951cc08f1405c3c31c
Taiye <taiyeajimati@gmail.com>
2022-08-04 01:00:58 +0400

    Last home commit

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 import hamb from "../../images/icon-hamburger.svg"
+import closeMenu from "../../images/icon-close-menu.svg"
 
 export default function Nav() {
+
+  const [toggle, setToggle] = useState(false)
+  const [closeIcon, setCloseIcon] = useState(false)
+
+  function toggleMenu() {
+    setToggle(prevToggle => !prevToggle)
+    setCloseIcon(prevClose => !prevClose)
+  }
+  function closMenu() {
+    setToggle(prevToggle => !prevToggle)
+    setCloseIcon(prevClose => !prevClose)
+  }
+
+
+
   return (
     <div className='Nav-bar pad'>
       <div className="logo-container"><h1 className="logo">crowdfund</h1></div>
       <div className="navigation">
-        <ul className="list-items">
+        <ul className={toggle ? "list-items" : "list-items  hide"} >
           <li><a href="#">About</a></li>
           <li><a href="#">Discover</a></li>
           <li><a href="#">Get Started</a></li>
         </ul>
         <div className="mobile-menu">
-          <img src={hamb} alt="hamburger" className="hamburger" />
+          <img src={hamb} alt="hamburger" className={toggle ? "hamburger hide" : "hamburger"} onClick={toggleMenu} />
+          <img src={closeMenu} alt="close-menu" className={closeIcon ? "close-menu" : "close-menu  hide"} onClick={closMenu} />
         </div>
       </div>
-    </div>
+    </div >
   )
 }
--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import './Tracker.css'
-import '../Card.css'
 export default function Tracker() {
   return (
     <div className='card tracker-wrapper'>
-        <div className="backed">
-            <h1 className="heading">$89,914</h1>
-            <p className="body">of $100,000 backed</p>
-        </div>
-        <div className="backers">
-            <h1 className="heading">5,007</h1>
-            <p className="body">total backers</p>
-        </div>
-        <div className="time-left">
-            <h1 className="heading">56</h1>
-            <p className="body">days left</p>
-        </div>
+      <div className="backed">
+        <h1 className="heading">$89,914</h1>
+        <p className="body">of $100,000 backed</p>
+      </div>
+      <div className="backers">
+        <h1 className="heading">5,007</h1>
+        <p className="body">total backers</p>
+      </div>
+      <div className="time-left">
+        <h1 className="heading">56</h1>
+        <p className="body">days left</p>
+      </div>
     </div>
   )
 }
