2303af52946961a1d8c35488a9f8764b85796cd2
Taiye Ajimati <taiyeajimati@gmail.com>
2022-08-04 17:40:09 +0400

    Last office commit

--- src/components/Tracker/Tracker.jsx
+++ src/components/Tracker/Tracker.jsx
@@ -3,15 +3,15 @@ import './Tracker.css'
 export default function Tracker() {
   return (
     <div className='card tracker-wrapper'>
-      <div className="backed">
+      <div className="backed border">
         <h1 className="heading">$89,914</h1>
         <p className="body">of $100,000 backed</p>
       </div>
-      <div className="backers">
+      <div className="backers border">
         <h1 className="heading">5,007</h1>
         <p className="body">total backers</p>
       </div>
-      <div className="time-left">
+      <div className="time-left border">
         <h1 className="heading">56</h1>
         <p className="body">days left</p>
       </div>
