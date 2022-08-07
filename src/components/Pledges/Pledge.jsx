8c5cc1393fdb55ecabd55fe9ac94ccf8910e388a
Taiye Ajimati <taiyeajimati@gmail.com>
2022-08-07 18:17:51 +0400

    Last office commit

--- src/components/Pledges/Pledge.jsx
+++ src/components/Pledges/Pledge.jsx
@@ -1,75 +1,117 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import '../btn/btn.css'
+import './Pledge.css'
+import '../btn/amount.css'
+import closeModalIcon from '../../images/icon-close-modal.svg'
 export default function Pledge() {
+    const [closeModal,setCloseModal]= useState(true)
+
+    function handleClick(){
+        setCloseModal(prev=>!prev)
+    }
     return (
-        <div className="pledges-wrapper pad">
+        <div className={closeModal?"pledges-wrapper pad":"hide"}>
+            <div className="Modalhead ">
             <h1 className="heading">Back this project</h1>
+            <img src={closeModalIcon} alt="Close Modal icon" className="closeModal" onClick={handleClick}/>
+            </div>
             <p className='body'>Want to support us in bringing Mastercraft Bamboo Monitor Riser out in the world?</p>
+           
+            {/* No-reward Container */}
             <div className="No-reward card">
                 <div className="head">
-                    <form>
-                        <label htmlFor='No-reward' className='NoReward'>
-                            <input type="radio" name="no-reward" value="Pledge with no reward" />
+                    
+                        <label className='NoReward'>
+                            <input type="radio" name="pledge" id='No-reward' value='No Reward'  />
                             Pledge with no reward
                         </label>
-                    </form>
-
-                    <h1 className="heading"> Pledge with no reward</h1>
-                </div>
-                <p className="body">Choose to support us without a reward if you simply believe in our project. As a backer,
-                    you will be signed up to receive product updates via email.</p>
-                <div className="bottom">
-                    <div className="slots">
+                        <div className="slots">
                         <h1 className="num">101</h1>
                         <p>left</p>
                     </div>
+
+                    </div>
+                <p className="body">Choose to support us without a reward if you simply believe in our project. As a backer,
+                    you will be signed up to receive product updates via email.</p>
+                <div className="bottom">
+                    
                 </div>
             </div>
 
+            {/* Bamboo Container */}
+
             <div className="bamboo-stand card">
-                <div className="head">
-                    <h1 className="heading">Bamboo Stand</h1>
-                    <h1 className="pledge">Pledge $25 or more</h1>
+                <div className="head">                
+                        <label className='Bamboo'>
+                            <input type="radio" name="pledge" id='Bamboo' value='Bamboo Stand'  />
+                            Bamboo Stand
+                        </label>
+                        <h1 className="pledge">Pledge $25 or more</h1>
+                        <div className="slots">
+                        <h1 className="num">101</h1>
+                        <p>left</p>
+                    </div>
+                    
                 </div>
                 <p className="body">You get an ergonomic stand made of natural bamboo. You've helped us launch our promotional campaign, and
                     you’ll be added to a special Backer member list.</p>
                 <div className="bottom">
-                    <div className="slots">
-                        <h1 className="num">101</h1>
-                        <p>left</p>
-                    </div>
+                    
                 </div>
             </div>
 
+
+            {/* Black Edition */}
             <div className="Black-Edition-Stand card ">
                 <div className="head">
-                    <h1 className="heading">Black Edition Stand</h1>
-                    <h1 className="pledge">Pledge $75 or more</h1>
+                
+                        <label className='BlackEdition'>
+                            <input type="radio" id='Black-edition' name="pledge" value='Black Edition Stand'  />
+                            Black Edition Stand
+                        </label>
+                     <h1 className="pledge">Pledge $75 or more</h1>
+                     <div className="slots">
+                        <h1 className="num">64</h1>
+                        <p>left</p>
+                    </div>
+                    
                 </div>
                 <p className="body">You get a Black Special Edition computer stand and a personal thank you. You’ll be added to our Backer
                     member list. Shipping is included.</p>
                 <div className="bottom">
-                    <div className="slots">
-                        <h1 className="num">64</h1>
-                        <p>left</p>
+                    <p className='Enter'>Enter your pledge</p>
+                    <div className="pledge-container">
+                      <form method="post" action="#">
+                        <input type="number" min='75' placeholder='$' className='amount' />
+                      </form>
+                    <button className="continue">Continue</button>
                     </div>
                 </div>
             </div>
 
+
+            {/* Mahogany Container */}
             <div className=" Mahogany-Special card ">
                 <div className="head">
-                    <h1 className="heading"> Mahogany Special Edition</h1>
-                    <h1 className="pledge">Pledge $200 or more</h1>
+                
+                        <label className='MahoganySpecial'>
+                            <input type="radio" id='Mahogany-special' name="pledge" value='Mahogany Special' />
+                            Mahogany Special Edition
+                        </label>
+                        <h1 className="pledge">Pledge $200 or more</h1>
+                        <div className="slots">
+                        <h1 className="num">0</h1>
+                        <p>left</p>
+                    </div>                    
+                   
                 </div>
                 <p className="body">You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You’ll be added
                     to our Backer member list. Shipping is included.</p>
                 <div className="bottom">
-                    <div className="slots">
-                        <h1 className="num">0</h1>
-                        <p>left</p>
-                    </div>
+                    
                 </div>
             </div>
+
         </div>
     )
 }
